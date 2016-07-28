2560f3de7ae4c1b0788496e7b2a60ae61da4280a
Alireza Andersen <alireza.andersen@gmail.com>
2016-07-28 13:09:36 -0600

    new structure with different layout

--- cityTempZip.js
+++ cityTempZip.js
@@ -1,9 +1,6 @@
 /* cityTempZip.js */
-/*
- * Creates <CityTempZip zip="80123"/> component
- *
- */
 
+require('fetch-bluebird');
 import React, { Component } from 'react';
 import {AppRegistry, Text, View} from 'react-native';
 
@@ -11,18 +8,62 @@ export default class CityTempZip extends Component{
   constructor(props){
     super(props);
     this.state = {temp:'',city:'',zip:''};
-    this.f();
+    //this.f();
   }
+
+  componentWillReceiveProps(nextProps,nextState){
+    console.log('calling componentWillReceiveProps');
+    console.log('nextProps=',nextProps);
+    console.log('nextState=',nextState);
+    this.f(nextProps.zip)
+  }
+
+  componentWillUpdate(nextProps,nextState){
+    console.log('calling componentWillUpdate',nextProps);
+    //console.log('cityTempZip.js','this.props.zip=',this.props.zip);
+  }
+
+  shouldComponentUpdate(nextProps,nextState){
+    console.log('calling shouldComponentUpdate');
+    console.log('this.props.zip =', this.props.zip);
+    console.log('nextProps.zip =', nextProps.zip);
+    //return nextProps.zip !== '' && this.props.zip !== nextProps.zip;
+    return true;
+  }
+
+  componentDidUpdate(){
+    console.log('calling componentDidUpdate');
+    //this.f();
+  }
+
+  componentWillMount(){
+    console.log('calling componentWillMount');
+  }
+
+  componentDidMount(){
+    console.log('calling componentDidMount');
+  }
+  /* using fetch-bluebird */
 
   f = function(){
     let self = this;
-    console.log("inside f()");
     var url = "http://outdooradventuretime.herokuapp.com/json/weather/?zip=" + this.props.zip;
     return fetch(url, {})
-      .then((res) => res.json())
-      .then((resJson) => self.setState({temp:resJson.temp,city:resJson.city,zip:resJson.zip}))
-      .catch((e) => e);
+           .call('json')
+           .then( (resJson) => self.setState({temp:resJson.temp,city:resJson.city,zip:resJson.zip}) )
+           .catch( (e) => e);
   }.bind(this);
+
+  /* using standard fetch */
+  // f = function(zip){
+  //   let self = this;
+  //   console.log("inside f()");
+  //   var url = "http://outdooradventuretime.herokuapp.com/json/weather/?zip=" + zip;
+  //   return fetch(url, {})
+  //     .then((res) => res.json())
+  //     .then((resJson) => self.setState({temp:resJson.temp,city:resJson.city,zip:resJson.zip}))
+  //     .catch((e) => e);
+  // }.bind(this);
 
   render(){
     return(
@@ -33,4 +74,4 @@ export default class CityTempZip extends Component{
   }
 }
 
-CityTempZip.defaultProps = {zip: 80123};
+CityTempZip.defaultProps = {zip: ''};
--- index.android.js
+++ index.android.js
@@ -1,15 +1,36 @@
 /* index.android.js */
 
 import React, { Component } from 'react';
-import {AppRegistry, Text, View} from 'react-native';
+import {AppRegistry, View, TextInput, Alert} from 'react-native';
 
 import CityTempZip from './cityTempZip';
 
 class OutdoorMobile extends Component{
+  constructor(props){
+    super(props)
+    this.state =  {zip:''}
+  }
+  isValidZip(input){
+    return /^\d{5}$/.test(input);
+  }
+  handleChange(event){
+    let inputText = event.nativeEvent.text;
+    if(this.isValidZip(inputText)){
+      this.setState({zip:inputText});
+    }
+    else{
+      Alert.alert('Invalid Zipcode',inputText);
+    }
+    console.log(event.nativeEvent.text);
+  }
+
   render(){
+    console.log('index.andrid.js','calling render()');
+    let textInputStyle = {height: 40};
     return(
       <View>
-        <CityTempZip zip="80223"/>
+        <TextInput style={textInputStyle} placeholder="Enter a zipcode" onSubmitEditing={this.handleChange.bind(this)}></TextInput>
+        <CityTempZip zip={this.state.zip}/>
       </View>
     );
   }
